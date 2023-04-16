9e464a14bdfb56167490493b816bb1c99fe08063
monicamrdam <monicamrdam@gmail.com>
2023-04-16 05:47:11 +0200

    modificado paquete cooperativa

---
+++ poo_cooperativa_java/Cooperativa.java
@@ -0,0 +1,15 @@
+import Interfaces.Interface;
+/**
+ * Write a description of class Cooperativa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cooperativa
+{
+   //Menú principal aplicación
+    public static void main(String[] args) {
+Interface.menu();
+    }
+    
+}
---
+++ poo_cooperativa_java/Interfaces/Interface.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+/**
+ * Write a description of class Interface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Interface
+{
+    public static void menu(){
+        System.out.println ("Hola Mundo");
+    }
+}
